3981fec65cd04e583bdd7b3c7b54daae28cb2621
3billiondevices <rumgool@ufl.edu>
2022-03-14 21:24:23 -0400

    Subtracts two numbers also

--- src/lab006.java
+++ src/lab006.java
@@ -1,11 +1,18 @@
 public class lab006 {
 
     public static void main(String[] args) {
-
+            // Adds numbers
 
         int num1 = 6;
         int num2 = 5;
 
         int num3 = num1 + num2;
+
+        //Subtracts Numbers
+
+        int num4 = num3 - num2;
+
+
+
     }
 }
